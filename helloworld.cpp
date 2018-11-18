12058e5b68ac645fffa6b831afa55d3268c8991b
JorgeACS <jorge_nintendo@hotmail.com>
2018-11-17 18:41:29 -0700

    Agrega comentarios a codigo

--- helloworld.cpp
+++ helloworld.cpp
@@ -10,7 +10,9 @@ int resta (int a, int b){
  	return a - b;
 }
 int main(){
+	// aqui voy a imprimir el string "hello world"
 	cout << "Hello world!" << endl;
+	// regresamos 0 porque si no explota
 	return 0;
 }
 
